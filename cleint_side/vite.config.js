86a33b89571e1ae9419812810f502f621c9784da
Ahmeddaud11 <shahwah3344@gmail.com>
2026-09-03 03:06:02 +0300

    first commit

---
+++ cleint_side/vite.config.js
@@ -0,0 +1,17 @@
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { defineConfig } from 'vite'
+
+const rootDir = path.dirname(fileURLToPath(import.meta.url))
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [tailwindcss(), react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(rootDir, './src'),
+    },
+  },
+})
