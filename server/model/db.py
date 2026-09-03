bfbe28d522b8619234fad0b719821e9046f7c4a4
Ahmeddaud11 <shahwah3344@gmail.com>
2026-09-03 22:44:21 +0300

    started servers side connected to database next setup schema and routes

---
+++ server/model/db.py
@@ -0,0 +1,22 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from utils.utils import settings
+
+db_url = settings.database_url
+if not db_url:
+    raise ValueError("Database URL is not set in the environment variables.")
+
+# Create the async engine using the database URL from settings
+engine = create_async_engine(db_url)
+
+
+# Create a sessionmaker for the async engine
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+
+# Dependency to get the database session
+async def get_db():
+    async with async_session() as session:
+        yield session
+
+
