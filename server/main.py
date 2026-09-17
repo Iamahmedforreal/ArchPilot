d3ea9cd54eab7e8bc3b39f9f8f694af879402be8
Ahmeddaud11 <shahwah3344@gmail.com>
2026-09-17 16:35:50 +0300

    added sumbitng router and services

--- server/main.py
+++ server/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 
+from routes.ai_route import router as ai_router
 from routes.projects import router as projects_router
 
 app = FastAPI()
 app.include_router(projects_router)
+app.include_router(ai_router)
 
 
 @app.get("/health")
